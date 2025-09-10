ad3d22b40bd316abad470692f11fc23179075e07
wxk <418306613@qq.com>
2025-09-11 00:14:20 +0800

    增加对中间件的注释

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -4,6 +4,12 @@ from django_ratelimit.exceptions import Ratelimited
 from django.http import JsonResponse
 from django.utils.deprecation import MiddlewareMixin
 
+"""
+验证码的限流配置中 block=True → 抛出 Ratelimited 异常 → 在进入 DRF 视图前就被 Django 拦截 
+→ DRF 的 EXCEPTION_HANDLER 无效 → 所以你必须在 Django 层（不是 DRF 层）捕获这个异常！
+所以选择 加一个中间件处理 Ratelimited 异常
+"""
+
 class RatelimitJsonMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
         if isinstance(exception, Ratelimited):
